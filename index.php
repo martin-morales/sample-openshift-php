8a84c76ad6b4c8ec919f66a14f16981da14f3934
Morales <mmoral04@harris.com>
2019-04-11 14:09:21 -0600

    Showing databases

--- index.php
+++ index.php
@@ -5,14 +5,29 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Sign In</title>
+	<title>Sample PHP App</title>
 </head>
 <body>
-	<h1>Sign In</h1>
+	<h1>Sample PHP App</h1>
 
 <?php
 	require_once("login_db.php");
 	echo "This is a sample PHP app. If this is showing, that means we correctly connected to the MYSQL database.";
+
+	$query = 'SHOW DATABASES';
+
+	$result = $conn->query($query);
+	?>
+
+	<table id="tableID">
+		<tr>
+			<?php
+			while ($row = mysqli_fetch_array($result)) {
+				echo "<td>"$row[0]."</td>"; 
+			}
+			?>
+		</tr>
+	</table>
 ?>
 </body>
 </html>
